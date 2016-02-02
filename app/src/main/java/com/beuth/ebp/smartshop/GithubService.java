80df6def36f6a24f443d3ccd11d24b4f46aa7b1f
jzaoueli <zaoueli.jihed@gmail.com>
2016-02-02 04:43:43 +0100

    go to smartShop after getting token

--- app/src/main/java/com/beuth/ebp/smartshop/GithubService.java
+++ app/src/main/java/com/beuth/ebp/smartshop/GithubService.java
@@ -1,6 +1,7 @@
 package com.beuth.ebp.smartshop;
 
 import retrofit.http.GET;
+import retrofit.http.Path;
 
 public interface GithubService {
 
@@ -13,7 +14,7 @@ public static final String ENDPOINT = "http://192.168.1.8:8080/SmartShopServer";
     @GET("/rest/api/getSessionID")
     Response sessionISResponse();
 
-    @GET("/rest/api/getTocken")
-    Response getTokenResponse();
+    @GET("/rest/api/getToken/{sessionid}")
+    Response getTokenResponse(@Path("sessionid") String sessionid);
 
 }
--- app/src/main/java/com/beuth/ebp/smartshop/LoginActiviy.java
+++ app/src/main/java/com/beuth/ebp/smartshop/LoginActiviy.java
@@ -1,12 +1,14 @@
 package com.beuth.ebp.smartshop;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import retrofit.ErrorHandler;
 import retrofit.RestAdapter;
@@ -17,8 +19,13 @@ import retrofit.RetrofitError;
  */
 public class LoginActiviy extends AppCompatActivity {
     ListReposTask getlistTask;
-    Button btn;
+    FetchToken getFetchToken;
+    Button btngoeBay;
+    Button btngoShop;
+    TextView txtgoeBay;
+    TextView txtgoShop;
     String sessionIDBody;
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +34,39 @@ public class LoginActiviy extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getlistTask = new ListReposTask();
         getlistTask.execute();
-        btn = (Button) findViewById(R.id.buttontoken);
+        btngoeBay = (Button) findViewById(R.id.buttongotoeBay);
+        btngoShop = (Button) findViewById(R.id.buttongotosmartshop);
+        txtgoeBay = (TextView) findViewById(R.id.textgotoebay);
+        txtgoShop = (TextView) findViewById(R.id.textgotosmartshop);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btngoeBay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //String sessionID = "qqsDAA**9fa7080e1520a471d237f6c7fffff5da";
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                String sessionID = sessionIDBody;
+                String ruName = "beuth-beuth5863-6795--zmfzuz";
+                String uriString = "https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&RUName="
+                        + ruName + "&SessID=" + sessionID + "";
+                Uri uri = Uri.parse(uriString); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+                btngoeBay.setVisibility(View.GONE);
+                txtgoeBay.setVisibility(View.GONE);
+                btngoShop.setVisibility(View.VISIBLE);
+                txtgoShop.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btngoShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sessionID = sessionIDBody;
+                getFetchToken = new FetchToken();
+                getFetchToken.execute();
+
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                intent.putExtra("userToken",token);
                 startActivity(intent);
             }
         });
@@ -66,6 +99,35 @@ public class LoginActiviy extends AppCompatActivity {
         protected void onPostExecute(Response repos) {
             super.onPostExecute(repos);
             sessionIDBody = repos.getBody();
+            Log.e("euloooooooooooo", "" + repos.getBody());
+        }
+    }
+    class FetchToken extends AsyncTask<String, Void, Response> {
+
+        @Override
+        protected Response doInBackground(String... params) {
+            GithubService githubService = new RestAdapter.Builder()
+                    .setEndpoint(GithubService.ENDPOINT)
+                    .setErrorHandler(new ErrorHandler() {
+                        @Override
+                        public Throwable handleError(RetrofitError cause) {
+                            retrofit.client.Response r = cause.getResponse();
+                            if (r != null && r.getStatus() == 405) {
+                                //      Toast.makeText(this, "Impossible d'effectuer cette action", Toast.LENGTH_SHORT).show();
+                            }
+                            return cause;
+                        }
+                    })
+                    .build().create(GithubService.class);
+
+            Response repoList = githubService.getTokenResponse(sessionIDBody);
+            return repoList;
+        }
+
+        @Override
+        protected void onPostExecute(Response repos) {
+            super.onPostExecute(repos);
+            token = repos.getBody();
             Log.e("euloooooooooooo", "" + repos.getBody());
         }
     }
