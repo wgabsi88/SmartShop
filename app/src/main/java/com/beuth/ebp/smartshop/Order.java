8ec3f4ba54e69d3c874c867a546fb625ec2770e8
wgabsi88 <wgabsi88@gmail.com>
2016-02-08 03:42:43 +0100

    order detail

--- app/src/main/java/com/beuth/ebp/smartshop/Order.java
+++ app/src/main/java/com/beuth/ebp/smartshop/Order.java
@@ -6,15 +6,32 @@ public class Order implements Serializable {
 
     private static final long serialVersionUID = -7060210544600464481L;
     private String id;
-    private String title;
-    private String quantity;
+    String orderid;
+    String productid;
+    String title;
+    String quantity;
+    String name;
+    String street;
+    int housenr;
+    int zip;
+    String city;
+    String email;
+    String phone;
 
-    public String getId() {
-        return id;
+    public String getOrderid() {
+        return orderid;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setOrderid(String orderid) {
+        this.orderid = orderid;
+    }
+
+    public String getProductid() {
+        return productid;
+    }
+
+    public void setProductid(String productid) {
+        this.productid = productid;
     }
 
     public String getTitle() {
@@ -33,5 +50,59 @@ public class Order implements Serializable {
         this.quantity = quantity;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHousenr() {
+        return housenr;
+    }
+
+    public void setHousenr(int housenr) {
+        this.housenr = housenr;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
