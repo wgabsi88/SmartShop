0112e4b35fcee1418462d17d44aa5e25690e7fc9
wgabsi88 <wgabsi88@gmail.com>
2016-02-08 04:20:39 +0100

    order detail

--- app/src/main/java/com/beuth/ebp/smartshop/Order_detail.java
+++ app/src/main/java/com/beuth/ebp/smartshop/Order_detail.java
@@ -2,10 +2,22 @@ package com.beuth.ebp.smartshop;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Order_detail extends AppCompatActivity {
-    String value;
+    private String id;
+
+    String productid;
+    String title;
+    String quantity;
+    String name;
+    String street;
+    int housenr;
+    int zip;
+    String city;
+    String email;
+    String phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,10 +25,52 @@ public class Order_detail extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if(extras !=null) {
-            value = extras.getString("id");
+            id = extras.getString("id");
+            Log.e("orderdetail",""+id);
+            productid = extras.getString("productid");
+            Log.e("productid",""+productid);
+            title = extras.getString("title");
+            Log.e("title",""+title);
+            quantity = extras.getString("quantity");
+            Log.e("quantity",""+quantity);
+            name = extras.getString("name");
+            Log.e("name",""+name);
+            street = extras.getString("street");
+            Log.e("street",""+street);
+            housenr = extras.getInt("housenr");
+            zip = extras.getInt("zip");
+            Log.e("zip",""+zip);
+            city = extras.getString("city");
+            Log.e("city",""+city);
+            email = extras.getString("email");
+            Log.e("email",""+email);
+            phone = extras.getString("phone");
+            Log.e("phone",""+phone);
+
         }
-        TextView txt = (TextView)findViewById(R.id.textviewproductname);
-        txt.setText(value);
+        TextView txtid = (TextView)findViewById(R.id.textviewproductname);
+        TextView txttitle = (TextView)findViewById(R.id.textviewproductname);
+        TextView txtquantity = (TextView)findViewById(R.id.textviewproductquantity);
+        TextView txtname = (TextView)findViewById(R.id.textviewrevievername);
+        TextView txtstreet = (TextView)findViewById(R.id.textviewrevieverstreet);
+        TextView txthousenr = (TextView)findViewById(R.id.textviewrevieverhousenr);
+        TextView txtzip = (TextView)findViewById(R.id.textviewrevieverzip);
+        TextView txtcity = (TextView)findViewById(R.id.textviewrevievercity);
+        TextView txtemail = (TextView)findViewById(R.id.textviewrevievermail);
+        TextView txtphone = (TextView)findViewById(R.id.textviewrevieverphone);
+
+
+
+        txtid.setText(id);
+        txttitle.setText(title);
+        txtquantity.setText(quantity);
+        txtname.setText(name);
+        txtstreet.setText(street);
+        txthousenr.setText(""+housenr);
+        txtzip.setText(""+zip);
+        txtcity.setText(city);
+        txtemail.setText(email);
+        txtphone.setText(phone);
     }
 
 }
